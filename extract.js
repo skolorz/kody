19d0b8639c29bf17a1843e158bf54fdadc4c9df9
Szymon Kolorz <skolorz@sksi.pl>
2017-03-18 22:48:54 +0000

    parsowanie okregow wyborczych:

--- extract.js
+++ extract.js
@@ -1,12 +1,25 @@
 var fs = require("fs"),
     fastCsv = require("fast-csv"),
     stream = fs.createReadStream("./kody.csv"),
-    parser = fastCsv();
+    parser = fastCsv(),
+	result = {};
 fastCsv
  .fromStream(stream, {headers : true, delimiter: ";"})
      .on("data", function(data){
-              console.log(data);
-               })
+		var wojewodztwo = data["WOJEWÓDZTWO"],
+            powiat = data["POWIAT"],
+            kod = data["KOD POCZTOWY"];
+        result[wojewodztwo] = result[wojewodztwo] || {},
+        result[wojewodztwo][powiat] = result[wojewodztwo][powiat] || {},
+        result[wojewodztwo][powiat][kod] = true;
+    })
  .on("end", function(){
-          console.log("done");
+          console.log(JSON.stringify(result));
            });
+/*
+{ 'KOD POCZTOWY': '85-460',
+  ADRES: 'ul. Słowicza',
+  'MIEJSCOWOŚĆ': 'Bydgoszcz',
+  'WOJEWÓDZTWO': 'Województwo kujawsko-pomorskie',
+  POWIAT: 'Miasto Bydgoszcz na prawach powiatu' }
+*/
