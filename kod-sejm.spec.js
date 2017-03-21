d06fd58020afcd3f8cba4a4213e6e0b47ede3053
Szymon Kolorz <skolorz@sksi.pl>
2017-03-21 20:31:49 +0000

    refaktor testów

--- kod-sejm.spec.js
+++ kod-sejm.spec.js
@@ -3,6 +3,7 @@ describe("Mapowanie kodów pocztowych na okręgi", function() {
     	kody = require("./data/kody.json"),
     	ks = require("./kod-sejm.js");
 
+describe("kod na okręg", function() {
  it("znajduje okręg po kodzie powiatu", function() {
     var okreg = ks.kodSejm("44-310", kody, okregi)
 	expect(okreg.nr).toBe("30");
@@ -12,11 +13,13 @@ describe("Mapowanie kodów pocztowych na okręgi", function() {
     var okreg = ks.kodSejm("44-240", kody, okregi)
 	expect(okreg.nr).toBe("30");
   });
+}
 
+describe("lista kodów na listę okręgów", function() {
  it("zlicza kody w okregu", function() {
     var result = ks.stats(["44-240","44-310"], kody, okregi)
 	expect(result).toEqual({ "30": 2});
   });
-
+}
 });
     
