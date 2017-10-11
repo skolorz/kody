e36685a65a893f158cd6a8fc458e61f30ad7abb9
Szymon Kolorz <skolorz@sksi.pl>
2017-10-11 23:17:07 +0200

    okregi razem

--- spec/kod-razem.spec.js
+++ spec/kod-razem.spec.js
@@ -1,14 +1,26 @@
-describe("Mapowanie kodów pocztowych na okręgi Razem", function() {
+describe("Mapowanie kodów pocztowych na okręgi Razem:", function() {
 
     var razem = require("../data/okregi-razem.json"),
         kody = require("../data/kody.json"),
         ks = require("../src/kod-razem.js")(kody, razem);
 
-    describe("kod na okręg", function() {
-        it("znajduje okręg po kodzie powiatu", function() {
-            var okreg = ks.okregByKod("44-310");
+    it("Katowicki", function() {
+        var okreg = ks.okregByKod("44-310");
 
-            expect(okreg.nazwa).toBe("Katowicki");
+        expect(okreg.nazwa).toBe("Katowicki");
+    });
+
+    it("Białostocki", function() {
+        var okreg = ks.okregByKod("16-001");
+
+        expect(okreg.nazwa).toBe("Białostocki");
+    });
+
+    describe("Statystyki", function (){
+        it("jeden kod", function() {
+            var okregi = ks.stats(["16-001"]);
+
+            expect(okregi[0].nazwa).toBe("Białostocki");
         });
     });
 });
